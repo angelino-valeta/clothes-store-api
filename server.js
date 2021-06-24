75112689cd27586b7d73f92c11beef5c420135e3
Angelino Valeta <angelinovaleta@gmail.com>
2021-06-24 22:12:37 +0100

    server

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require('./app')
+const dotenv = require('dotenv')
+const { sequelize } = require('./models')
+
+
+process.on('uncaughtException', err => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Desligando o servidor devido a um erro ocorrido.`);
+  process.exit(1)
+})
+
+// Configurando variaveis de ambiente
+dotenv.config({ path: './config/config.env' });
+
+
+const PORT = process.env.PORT || 5000
+
+// Conectando a base de dados e o ligando o servidor
+sequelize.sync({force: true})
+	.then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`)
+    })
+	})
+
